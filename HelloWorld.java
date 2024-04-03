faa59d96939861b0b4c00c2007cffef27f07c578
Nagaya Hitoshi <nag.hitoshi1011@gmail.com>
2024-04-03 22:07:00 +0900

    Adding an output method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,36 @@
 /* Javaの練習用コード
  * ターミナルに表示させるコードの実験 */
 
-// 単出力
+// 出力用クラス
 public class HelloWorld
 {
-  public static void main(String[] args) {
+  //文字列出力
+  static void showHello(){
     System.out.println("HelloWorld");
+    System.out.println("new line\n");
+    System.out.print("No line break. ");
+    System.out.println("(^^)");
+  }
+
+  //計算結果出力
+  static void showInteger(int a, int b){
+    int sum, difference, product, quotient;
+    sum = a + b;
+    difference = a - b;
+    product = a * b;
+    quotient = a / b;
+
+    System.out.println("合計 = " + sum);
+    System.out.println("差 = " + difference);
+    System.out.println("積 = " + product);
+    System.out.println("商 = " + quotient);
+  }
+
+  public static void main(String[] args) {
+    showHello();
+    int a, b;
+    a = 8;
+    b = 3;
+    showInteger(a, b);
   }
 }
