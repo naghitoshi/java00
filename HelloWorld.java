0ba401b6ec6177397bca76e1f1919f2a5f58bf20
Nagaya Hitoshi <nag.hitoshi1011@gmail.com>
2024-04-06 22:59:14 +0900

    Add method 'showDate, showEncoding, showCharacter'

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,18 +2,26 @@
  * ターミナルに表示させるコードの実験 */
 
 // 出力用クラス
+
+import java.time.LocalDate;
+
 public class HelloWorld
 {
   //文字列出力
   static void showHello(){
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println("output of method: " + methodName);
     System.out.println("HelloWorld");
-    System.out.println("new line\n");
-    System.out.print("No line break. ");
+    System.out.println("insert a new line\n");
+    System.out.print("No break. ");
     System.out.println("(^^)");
+    System.out.println(); 
   }
 
   //計算結果出力
   static void showInteger(int a, int b){
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println("output of method: " + methodName);
     int sum, difference, product, quotient;
     sum = a + b;
     difference = a - b;
@@ -24,13 +32,51 @@ public class HelloWorld
     System.out.println("差 = " + difference);
     System.out.println("積 = " + product);
     System.out.println("商 = " + quotient);
+    System.out.println("合計=" + sum + ", 差=" + difference + 
+                        ", 積=" + product + ", 商=" + quotient);
+    System.out.println();                    
   }
 
+  //日付の出力
+  static void showDate(){
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println("output of method: " + methodName);
+    /* 
+    java.time.LocalDate ld = java.time.LocalDate.now();
+    System.out.println("Today: " + ld);
+    */
+
+    LocalDate ld2 = LocalDate.now();  //importによってエラーが起きない
+    System.out.println("Today: " + ld2);
+    System.out.println();
+  }
+
+  //エンコーディングの出力
+  static void showEncoding(){
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println("output of method: " + methodName);
+    System.out.println("Encoding: " + System.getProperty("file.encoding"));
+    System.out.println();
+  }
+
+  //文字の出力
+  static void showCharacter(){
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println("output of method: " + methodName);
+    char c1, c2, c3;
+    c1 = 'a';
+    c2 = 0x0061;
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(0x0061); //直接入力すると、数値として扱われる
+    System.out.println('\u0061');
+    System.out.println();
+  }
   public static void main(String[] args) {
     showHello();
-    int a, b;
-    a = 8;
-    b = 3;
-    showInteger(a, b);
+    showInteger(8, 3);
+    showDate();
+    showEncoding();
+    showCharacter();
   }
 }
--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Java HotSpot(TM) 64-Bit Server VM (build 22+36-2370, mixed mode, sharing)
 `"HelloWorld"`などの文字列を出力する
 - `showInteger`(メソッド)   
 整数の計算結果を出力する
+- `showDate`(メソッド)  
+日付の出力
+- `showEncoding`(メソッド)  
+エンコーディングの出力
+- `showCharacter`(メソッド)  
+文字の出力
 
 ## クラス、メソッド、フィールド
 - クラス：特定の目的を達成するのに必要なものをまとめたもの  
@@ -59,6 +65,57 @@ public static void main(String[] args){
 ### public static
 - public：このメソッドがすべてのクラスからアクセス可能なことを宣言
 - static：このメソッドがクラスから作成したオブジェクトのメンバーではなく、クラスそのもののメンバーである事を宣言。
+
+## import
+importの宣言によって、クラス名だけで記述することができるようになる。
+`String`, `System`クラスなどは`java.lang`パッケージに含まれることからimportを宣言しなくても使用できる。
+```
+import パッケージ名.クラス名
+
+//例
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+//同じパッケージに含まれるクラスをまとめてimport宣言
+import java.util.regex.*;
+```
+
+## エンコーディング
+`System.getProperty("file.encoding")`によってデフォルトのエンコーディング方式を確認できる。
+```
+//エンコーディングを指定したコンパイル
+> javac -encoding エンコーディング名 ソースファイル名
+> javac -J-Dfile.encoding=エンコーディング名 ソースファイル名
+
+//エンコーディングを指定してプログラムを実行
+> java -Dfile.encoding=エンコーディング名 クラスファイル名
+```
+- エンコーディング名  
+以下は日本でよく使われるエンコーディング
+  - US-ASCII  
+  128文字の組み合わせを提供する米国の7ビットエンコーディング方式。基本的な英数字と一部の特殊文字をカバーしている。
+  - UTF-8  
+  可変長のエンコーディング方式であり、英数字や米国のASCII文字に関しては1バイトでエンコードされるが、それ以外の文字に関しては2〜4バイトの可変長でエンコードされる。日本語も含まれており、広くサポートされている。
+  - EUC-JP  
+  Extended Unix Code-JPの略で、日本語の文字エンコーディング方式の1つ。ASCII文字に関しては1バイトでエンコードされ、日本語の漢字や仮名に関しては2バイトでエンコードされる。
+  - Shift_JIS  
+  主に日本語の文字をエンコードするためのエンコーディング方式。ASCII文字に関しては1バイトでエンコードされ、ほとんどの日本語の文字に関しては2バイトでエンコードされる。
+  - windows-31j  
+  主にMicrosoft Windowsで使用される日本語の文字エンコーディング方式。Shift_JISと同様に、ASCII文字に関しては1バイトでエンコードされ、ほとんどの日本語の文字に関しては2バイトでエンコードされる。
+
+## 文字リテラル
+文字リテラルは一つの文字の値。複数の文字からなる値は文字列リテラルと呼ばれ区別される。
+```
+char c1 = 'a';
+char c2 = 0x0061;
+
+System.out.println(c1); //a
+System.out.println(c2); //a
+
+//直接入力する場合は'\uXXXX'のように記述する
+System.out.println(0x0061); //97
+System.out.println('\u0061'); //a
+```
 
 ## 参考
 - [Java入門](https://www.javadrive.jp/start/#section_install) (2024/04/02)
